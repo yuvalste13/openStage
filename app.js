b2235d231bde709b6e4d6ee17133b18b23617d36
ofryma <ofry60000@gmail.com>
2022-04-25 18:33:02 +0300

    global css styles and app.js update

--- app.js
+++ app.js
@@ -4,31 +4,31 @@ const request = require("request");
 const bodyParser = require("body-parser");
 var $ = require("jquery");
 const stages = require(__dirname + "/stages.js");
-const ld = require("lodash");
+const lodash = require("lodash");
 
-
-const stageList = stages.stages;
 
 const app = express();
 app.set('view engine', 'ejs');
-
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+const stageList = stages.stages;
 const CityList = ["Ashdod","Ashkelon","beer sheva"];
+
 
 app.get("/",function(req,res){
   res.render("index",{CityList: CityList, StageList: stageList});
 });
 
-// app.post("/",function(req,res){
-//   var day = req.body.button;
-//   DayList = [day];
-//
-//   res.render("index",{CityList: CityList,DayList: DayList});
-//
-// });
-
+app.get("/stages/:stageName",function(req,res){
+  const requestedTitle = lodash.lowerCase(req.params.stageName);
+  stageList.forEach(function(stage){
+    if (lodash.lowerCase(stage.Name) === requestedTitle){
+      res.render("selection",{CityList: CityList, StageList: stageList ,Stage: stage});
+    }
+  });
+});
 
 
 // listening to a dinamic port (for using heroku) and on our localhost at port 3000
