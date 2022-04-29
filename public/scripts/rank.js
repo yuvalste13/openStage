de750eb22fb18cc23ae6ff8d6ce318cc06eb91eb
ofryma <ofry60000@gmail.com>
2022-04-29 10:39:28 +0300

    favicon ico bug fixed

--- public/scripts/rank.js
+++ public/scripts/rank.js
@@ -2,13 +2,15 @@
 let button_names = ["one","two","three","four","five"];
 let buttons = [];
 
-// buttons = (document.querySelector("i.star"))
+
+// finding all the buttons with an id of a number
 for(let i=0 ; i<5 ; i++){
   buttons.push(document.querySelector("#" + button_names[i]));
-
 }
 
-
+// creating an event listener for each button
+// every time the mouse is over a button the index of the button is returned
+// then lighting all the lower index and turn of all the higere ones
 for(let i=0; i < buttons.length ; i++){
   buttons[i].addEventListener("mouseover",function(event){
     for(let j=0; j <= buttons.length ; j++){
